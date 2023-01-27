3988cb8fe64a5b5ac79603ff5cc8cd73a7dc94cf
Burak Cihan <burakcihan622@gmail.com>
2023-01-27 19:29:17 +0300

    styles klasör oluşturdu & global varaiables oluşturuldu & color palet oluşturuldu

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 // Reset SCSS
-import './reset.scss';
+import './styles/reset.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
