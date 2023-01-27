a5b0988de0c39ebe21c5883ca8dd7e71ce6f6dde
Burak Cihan <burakcihan622@gmail.com>
2023-01-27 18:47:32 +0300

    SCSS import edildi & reset css aktif edildi.

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 
+// Reset SCSS
+import './reset.scss';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <>
     <App />
