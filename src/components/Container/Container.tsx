a71091e6dcaf4557bd26e0a98f35e5ac807099fa
Burak Cihan <burakcihan622@gmail.com>
2023-01-27 20:26:37 +0300

    Header Component oluşturuldu & geliştirildi

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import styles from './container.module.scss';
+import Header from '../Header/Header';
 
 const Container = () => {
-  return <div className={styles.container}></div>;
+  return (
+    <div className={styles.container}>
+      <Header />
+    </div>
+  );
 };
 
 export default Container;
